a2e5a554d09ebe39a823ed68c32201d5826f88cb
thanhle <thanhle@hearti.io>
2018-12-17 00:19:53 +0700

    Create category data

--- server/seeders/20181216170632-create-category.js
+++ server/seeders/20181216170632-create-category.js
@@ -1,7 +1,6 @@
 'use strict';
 
-import {userRepository} from '../repositories';
-import {User} from '../models';
+import {categoryRepository, Op} from '../repositories';
 import Download from '../helpers/Download';
 import Uuid from 'uuid';
 import Path from 'path';
@@ -14,14 +13,10 @@ module.exports = {
             for (let i = 0; i < 10; i++) {
                 const imageName = `${Uuid.v4()}.jpg`;
                 await Download.file('https://picsum.photos/200?random', pathUploads, imageName);
-                await userRepository
+                await categoryRepository
                     .create({
-                        email: Faker.internet.email(),
-                        password: 'abc123',
-                        firstName: Faker.name.firstName(),
-                        lastName: Faker.name.lastName(),
-                        role: User.Roles.USER,
-                        avatar: imageName
+                        title: Faker.name.title(),
+                        image: imageName,
                     });
             }
 
@@ -34,7 +29,13 @@ module.exports = {
 
     down: async (queryInterface, Sequelize) => {
         try {
-            await userRepository.delete({where: {'role': User.Roles.USER}});
+            await categoryRepository.delete({
+                where: {
+                    id: {
+                        [Op.ne]: null
+                    }
+                }
+            });
             return true;
         } catch (error) {
             console.log(error);
